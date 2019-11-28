585c7f290f2d91cb32ffa997bb16424bb8fd0ec5
Marek Sebera <marek.sebera@gmail.com>
2019-11-28 21:43:32 +0100

    Basic IdP metadata functions, NiaExtensions copy from eidentita-example

--- src/OMSAML2.php
+++ src/OMSAML2.php
@@ -2,7 +2,153 @@
 
 namespace OMSAML2;
 
+use Exception;
+use SAML2\Constants;
+use SAML2\DOMDocumentFactory;
+use SAML2\XML\md\EntityDescriptor;
+use SAML2\XML\md\IDPSSODescriptor;
+
+/**
+ * @package OMSAML2
+ */
 class OMSAML2
 {
-    
+    protected static $idp_metadata_url = null;
+    protected static $idp_metadata_contents = null;
+
+    /**
+     * Returns associative array of found Login URLs
+     * keys are binding constants, such as Constants::BINDING_HTTP_POST
+     *
+     * @param EntityDescriptor|null $idp_descriptor
+     * @return array
+     * @throws Exception
+     * @see Constants
+     *
+     */
+    public static function extractSSOLoginUrls(?EntityDescriptor $idp_descriptor = null): array
+    {
+        return self::extractSSOUrls(false, $idp_descriptor);
+    }
+
+    /**
+     * @param bool $extract_logout_urls
+     * @param EntityDescriptor $idp_descriptor
+     * @return array
+     * @throws Exception
+     */
+    public static function extractSSOUrls(bool $extract_logout_urls = false, ?EntityDescriptor $idp_descriptor = null): array
+    {
+        if (empty($idp_descriptor)) {
+            $idp_descriptor = self::getIdpDescriptor();
+        }
+
+        $idp_sso_descriptor = false;
+        foreach ($idp_descriptor->getRoleDescriptor() as $role_descriptor) {
+            if ($role_descriptor instanceof IDPSSODescriptor) {
+                $idp_sso_descriptor = $role_descriptor;
+            }
+        }
+
+        $found = [];
+
+        if ($idp_sso_descriptor instanceof IDPSSODescriptor) {
+            foreach ($extract_logout_urls ? $idp_sso_descriptor->getSingleLogoutService() : $idp_sso_descriptor->getSingleSignOnService() as $descriptorType) {
+                if (empty($descriptorType->getBinding())) {
+                    continue;
+                }
+                $found[$descriptorType->getBinding()] = $descriptorType->getLocation();
+            }
+        }
+
+        return $found;
+    }
+
+    /**
+     * Returns EntityDescriptor instance or false, if metadata could not be fetched
+     * throws exception in case of invalid or dangerous XML contents
+     *
+     * @return EntityDescriptor|false
+     * @throws Exception
+     */
+    public static function getIdpDescriptor(): EntityDescriptor
+    {
+        $metadata_string = self::getIdPMetadataContents();
+        if ($metadata_string) {
+            return false;
+        }
+        $metadata_dom = DOMDocumentFactory::fromString($metadata_string);
+        return new EntityDescriptor($metadata_dom->documentElement);
+    }
+
+    /**
+     * Returns cached or freshly retrieved IdP metadata as a string, or false
+     *
+     * @param string|null $url
+     * @return false|string
+     * @throws Exception
+     */
+    public static function getIdPMetadataContents(?string $url = null): ?string
+    {
+        if (!empty($url)) {
+            self::setIdPMetadataUrl($url);
+        }
+        if (empty(self::getIdPMetadataUrl())) {
+            throw new Exception("IdP Metadata URL not yet configured");
+        }
+        if (empty(self::$idp_metadata_contents)) {
+            $idp_metadata_contents_fresh = file_get_contents(self::getIdPMetadataUrl());
+            self::setIdPMetadataContents($idp_metadata_contents_fresh);
+        }
+        return self::$idp_metadata_contents;
+    }
+
+    /**
+     * Sets metadata content cache to provided string contents or null, if provided value is empty
+     *
+     * @param string $contents
+     */
+    public static function setIdPMetadataContents(?string $contents)
+    {
+        self::$idp_metadata_contents = empty($contents) ? null : $contents;
+    }
+
+    /**
+     * Retrieves currently configured IdP Metadata URL or null if current value is empty
+     *
+     * @return null|string
+     */
+    public static function getIdPMetadataUrl(): ?string
+    {
+        return empty(self::$idp_metadata_url) ? null : self::$idp_metadata_url;
+    }
+
+    /**
+     * If provided URL is not string or is empty, will set null
+     *
+     * @param string $url
+     */
+    public static function setIdPMetadataUrl(string $url): void
+    {
+        if ($url != self::$idp_metadata_url) {
+            // empty metadata contents cache if URL changes
+            self::$idp_metadata_contents = null;
+        }
+        self::$idp_metadata_url = empty($url) ? null : $url;
+    }
+
+    /**
+     * Returns associative array of found Logout URLs
+     * keys are binding constants, such as Constants::BINDING_HTTP_REDIRECT
+     *
+     * @param EntityDescriptor|null $idp_descriptor
+     * @return array
+     * @throws Exception
+     * @see Constants
+     *
+     */
+    public static function extractSSOLogoutUrls(?EntityDescriptor $idp_descriptor = null): array
+    {
+        return self::extractSSOUrls(true, $idp_descriptor);
+    }
 }
